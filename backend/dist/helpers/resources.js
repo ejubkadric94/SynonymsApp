4273738d10afa3b79bf1f6fb3b462c12e0ca092a
ejubkadric <ejubkadric@gmail.com>
2020-03-03 14:22:25 +0100

    Remove dist

--- backend/dist/helpers/resources.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var INVALID_WORD_RESPONSE = exports.INVALID_WORD_RESPONSE = 'Provided word is invalid';
-var INVALID_SYNONYMS_RESPONSE = exports.INVALID_SYNONYMS_RESPONSE = 'Provided synonyms are not valid';
-
-var HTTP_BAD_REQUEST = exports.HTTP_BAD_REQUEST = 400;
--- backend/dist/helpers/responseHelper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var prepareError = exports.prepareError = function prepareError(error) {
-  return { message: error };
-};
