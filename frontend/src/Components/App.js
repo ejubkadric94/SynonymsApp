46552cc998e7ec9dfb064350aca3ded08d44154a
ejubkadric <ejubkadric@gmail.com>
2020-03-03 15:59:56 +0100

    Handle mobile style

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.css';
 import Divider from './Divider/Divider';
 import AddSynonymsContainer from './AddSynonymsContainer/AddSynonymsContainer';
 import SearchSynonymsContainer from './SearchSynonymsContainer/SearchSynonymsContainer';
 import ResetStorageButton from './ResetStorageButton/ResetStorageButton';
+import './App.css';
+import './MobileApp.css';
 
 function App() {
   return (
